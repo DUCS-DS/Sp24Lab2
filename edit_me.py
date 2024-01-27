769fb2bccc0bf30cd5af3269d60e5617b51bee07
Scott Simmons <ssimmons1331@gmail.com>
2024-01-27 16:00:44 -0600

    initial commit

---
+++ edit_me.py
@@ -0,0 +1,14 @@
+#
+# PLEASE ADD YOUR NAME HERE
+#
+
+def monotonic(lst):
+    """Return True if lst is monotonic; return False, otherwise."""
+    #
+    # YOUR IMPLEMENTATION GOES HERE
+    #
+
+#
+# Feel free to replace these comments with
+# code that tests your function monotonic.
+#
